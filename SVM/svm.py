66794571012923f872501f23aaaba51bc8c08886
Rochakdh <rochakdh@gmail.com>
2021-07-16 18:29:16 +0545

    Comparision with KNN

--- SVM/svm.py
+++ SVM/svm.py
@@ -21,13 +21,9 @@ y_pred = clf.predict(x_test)
 acc = metrics.accuracy_score(y_test, y_pred)
 print(acc)
 
+#comparing With KNN
 clf = KNeighborsClassifier(n_neighbors=9)
 clf.fit(x_train, y_train)
 y_pred = clf.predict(x_test)
 acc = metrics.accuracy_score(y_test, y_pred)
 print(acc)
-
-
-
-#comparing With KNN
-
